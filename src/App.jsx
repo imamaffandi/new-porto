19932a7b71532086cccf8a7c90bd12142829a245
unknown <imamaffandi715@gmail.com>
2024-09-05 22:55:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-light">Halo mama</div>;
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Projects from "./Projects";
+import Footer from "./Footer";
+
+export { Navbar, About, Tech, Projects, Footer };
